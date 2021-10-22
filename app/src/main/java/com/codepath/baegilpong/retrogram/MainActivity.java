cb6a0993435b8462e361724dd9319877094832c7
Phong Nguyen <84949014+Baegilpong@users.noreply.github.com>
2021-10-22 19:39:01 -0400

    Post Recycler View and fragment implement

--- app/src/main/java/com/codepath/baegilpong/retrogram/MainActivity.java
+++ app/src/main/java/com/codepath/baegilpong/retrogram/MainActivity.java
@@ -12,7 +12,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.codepath.baegilpong.retrogram.fragments.ComposeFragment;
+import com.codepath.baegilpong.retrogram.fragments.PostsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
     private BottomNavigationView bottomNavigationView;
     private Button btnSignout;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
         androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.header,null);
-        actionBar.setCustomView(view);
+        View view1 = inflater.inflate(R.layout.header,null);
+        actionBar.setCustomView(view1);
 
         btnSignout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,22 +53,30 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment;
+                Fragment fragment = new Fragment();
                 switch (item.getItemId()) {
                     case R.id.action_home:
+                        // TODO: update fragment
                         Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        fragment = new PostsFragment();
                         break;
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
+                        // TODO: update fragment
                         Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
                     default:
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     private void goLoginActivity () {
